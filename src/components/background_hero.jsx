c003c75e96612bdddc20255e11da374d34a954d3
Bagas Cahyo Nugroho <bagascahyo19n@gmail.com>
2025-05-18 12:11:54 +0700

    bg hero addition

--- src/components/background_hero.jsx
+++ src/components/background_hero.jsx
@@ -8,11 +8,28 @@ const BackgroundHero = () => {
       <motion.div
         initial={{ translateX: "100vw", opacity: 0 }}
         animate={{ translateX: "60vw", opacity: 1 }}
+        transition={{ duration: 3, ease: "easeOut" }}
+        className="absolute top-0 right-5 w-screen h-screen -z-10"
+        style={{
+          clipPath: "polygon(100% -200%, 0% 100%, 100% 100%)",
+          backgroundImage:
+            "linear-gradient(rgba(30,55,103,1), rgba(47,77,133,1))",
+          backgroundPosition: "35% 50%",
+          backgroundSize: "80%",
+        }}
+      />
+
+      <motion.div
+        initial={{ translateX: "100vw", opacity: 0 }}
+        animate={{ translateX: "60vw", opacity: 1 }}
         transition={{ duration: 2, ease: "easeOut" }}
         className="absolute top-0 left-0 w-screen h-screen -z-10"
         style={{
           clipPath: "polygon(100% -200%, 0% 100%, 100% 100%)",
-          backgroundColor: "#2F4D85",
+          backgroundImage:
+            "linear-gradient(rgba(30,55,103,0.5), rgba(47,77,133,1)),url('src/assets/image/bd.jpg')",
+          backgroundPosition: "35% 50%",
+          backgroundSize: "80%",
         }}
       />
 
@@ -30,6 +47,47 @@ const BackgroundHero = () => {
         <Live2DCanvas />
       </motion.div>
       {/* add a triangle foreground here */}
+      <motion.div
+        initial={{ translateX: "100vw", opacity: 0 }}
+        animate={{ translateX: "60vw", opacity: 1 }}
+        transition={{ duration: 2, ease: "easeOut" }}
+        className="absolute top-0 right-50 w-screen h-screen"
+        style={{
+          clipPath: "polygon(100% 50%, 0% 100%, 100% 100%)",
+          backgroundColor: "#2B2B2B",
+        }}
+      />
+      <div className="flex flex-row absolute bottom-20 right-10 gap-4">
+        <a
+          href="https://vgen.co/yozzun"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-19 !bg-white"
+          >
+            <img
+              src="https://help.vgen.co/hc/article_attachments/13004231445911"
+              alt=""
+            />
+          </motion.button>
+        </a>
+        <a
+          href="https://x.com/Cahyoz9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="!bg-white rounded-lg p-2 w-19 flex items-center justify-center"
+          >
+            <img src="https://static.cdnlogo.com/logos/x/9/x.svg" alt="X" />
+          </motion.button>
+        </a>
+      </div>
     </div>
   );
 };
